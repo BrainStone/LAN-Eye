c654f57274beba682c0ab85ebda9f49b2e1359ce
BrainStone <yannick@tedworld.de>
2023-10-29 01:12:37 +0200

    Compression stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,26 @@ int main() {
 	// ==== Logging ====
 	const std::string log_path = "log";
 
+	// Application Log
+	drogon::app().setLogPath(log_path).setLogLevel(trantor::Logger::kInfo).setLogLocalTime(true);
+
 	// Access Log
 	Json::Value accessLoggerConfig;
 	accessLoggerConfig["log_path"] = log_path;
 	drogon::app().addPlugin("drogon::plugin::AccessLogger", {}, accessLoggerConfig);
 
-	// Application Log
-	drogon::app().setLogPath(log_path).setLogLevel(trantor::Logger::kInfo);
-
 	// ==== Application ====
 	// Listen Address
 	drogon::app().addListener("0.0.0.0", 8080);
 
-	// Worker threads
+	// Worker Threads
 	drogon::app().setThreadNum(16);
+
+	// Compression Stuff
+	drogon::app().enableGzip(true).enableBrotli(true);
+
+	// Disable Sessions
+	drogon::app().disableSession();
 
 	// Start Application
 	drogon::app().run();
