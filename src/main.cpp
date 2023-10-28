a3b0941389f202d5de2467b1da79b4b3de922dfa
BrainStone <yannick@tedworld.de>
2023-10-28 23:07:27 +0200

    Building to make sure everything is ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include "drogon/drogon.h"
+
 using namespace drogon;
+
 int main() {
-  app()
-      .setLogPath("./log")
-      .setLogLevel(trantor::Logger::kDebug)
-      .addListener("0.0.0.0", 8080)
-      .setThreadNum(16)
-      .run();
+	app().setLogPath("./log").setLogLevel(trantor::Logger::kDebug).addListener("0.0.0.0", 8080).setThreadNum(16).run();
 }
