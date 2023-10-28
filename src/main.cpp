5f202a1003e4cd10fbac60aef1a3fb6338aa7c4d
BrainStone <yannick@tedworld.de>
2023-10-29 00:45:23 +0200

    Structure startup a bit more

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include "drogon/drogon.h"
 
-using namespace drogon;
-
 int main() {
-	app().setLogPath("./log").setLogLevel(trantor::Logger::kDebug).addListener("0.0.0.0", 8080).setThreadNum(16).run();
+	// ==== Logging ====
+	const std::string log_path = "log";
+
+	// Access Log
+	Json::Value accessLoggerConfig;
+	accessLoggerConfig["log_path"] = log_path;
+	drogon::app().addPlugin("drogon::plugin::AccessLogger", {}, accessLoggerConfig);
+
+	// Application Log
+	drogon::app().setLogPath(log_path).setLogLevel(trantor::Logger::kInfo);
+
+	// ==== Application ====
+	// Listen Address
+	drogon::app().addListener("0.0.0.0", 8080);
+
+	// Worker threads
+	drogon::app().setThreadNum(16);
+
+	// Start Application
+	drogon::app().run();
 }
