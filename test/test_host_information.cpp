795e0eed9d347d348beaae4c95a28743c2c5377c
BrainStone <yannick@tedworld.de>
2023-11-13 04:22:00 +0100

    Parametrize (De)Serialization tests

--- test/test_host_information.cpp
+++ test/test_host_information.cpp
@@ -7,20 +7,6 @@
 #include "json/reader.h"
 #include "json/writer.h"
 
-const std::tuple<host, std::string, std::string> host_test_cases[]{
-    {host{.ip = boost::asio::ip::make_address("1.2.3.4")}, R"({"hostnames":[],"ip":"1.2.3.4"})",
-     R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
-    {host{.ip = boost::asio::ip::make_address("1.2.3.4"), .mac = "01:23:45:67:89:ab"},
-     R"({"hostnames":[],"ip":"1.2.3.4","mac":"01:23:45:67:89:ab"})",
-     R"(<host><status state="up" reason="arp-response" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><address addr="01:23:45:67:89:AB" addrtype="mac"/><hostnames></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
-    {host{.ip = boost::asio::ip::make_address("1.2.3.4"), .hostnames = {"example.com"}},
-     R"({"hostnames":["example.com"],"ip":"1.2.3.4"})",
-     R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames><hostname name="example.com" type="PTR"/></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
-    {host{.ip = boost::asio::ip::make_address("1.2.3.4"), .hostnames = {"example.com", "example.local", "aaaaaaaaa"}},
-     R"({"hostnames":["example.com","example.local","aaaaaaaaa"],"ip":"1.2.3.4"})",
-     R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames><hostname name="example.com" type="PTR"/><hostname name="example.local" type="PTR"/><hostname name="aaaaaaaaa" type="PTR"/></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
-};
-
 class TestChronoHelpers : public ::testing::TestWithParam<std::intmax_t> {
 protected:
 	using system_tp_seconds_t = std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>;
@@ -63,44 +49,78 @@ TEST(TestChronoHelpers, MakeTimePointNow) {
 	          make_time_point<std::chrono::steady_clock::time_point>(steady_now.time_since_epoch().count()));
 }
 
-TEST(TestHost, XmlDeserialization) {
-	Json::Reader reader;
+using TestHost_testcase_t = std::tuple<host, std::string, std::string>;
 
-	for (const std::tuple<host, std::string, std::string>& test : host_test_cases) {
-		host host;
-		pugi::xml_document doc;
+class TestHost : public ::testing::TestWithParam<TestHost_testcase_t> {
+protected:
+	static Json::Reader reader;
+	static Json::StreamWriterBuilder builder;
+};
 
-		ASSERT_TRUE(doc.load_string(std::get<2>(test).c_str()));
-
-		doc.child("host") >> host;
-
-		EXPECT_EQ(std::get<0>(test), host);
-	}
-}
-
-TEST(TestHost, JsonDeserialization) {
-	Json::Reader reader;
-
-	for (const std::tuple<host, std::string, std::string>& test : host_test_cases) {
-		host host;
-		Json::Value root;
-
-		ASSERT_TRUE(reader.parse(std::get<1>(test), root));
-
-		root >> host;
-
-		EXPECT_EQ(std::get<0>(test), host);
-	}
-}
-
-TEST(TestHost, JsonSerialization) {
+Json::StreamWriterBuilder createBuilder() {
 	Json::StreamWriterBuilder builder{};
 	builder["indentation"] = "";
 
-	for (const std::tuple<host, std::string, std::string>& test : host_test_cases) {
-		Json::Value root;
-		root << std::get<0>(test);
-
-		EXPECT_EQ(std::get<1>(test), Json::writeString(builder, root));
-	}
+	return builder;
 }
+
+Json::Reader TestHost::reader{};
+Json::StreamWriterBuilder TestHost::builder = createBuilder();
+
+TEST_P(TestHost, XmlDeserialization) {
+	TestHost_testcase_t test = GetParam();
+
+	host host;
+	pugi::xml_document doc;
+
+	ASSERT_TRUE(doc.load_string(std::get<2>(test).c_str()));
+
+	doc.child("host") >> host;
+
+	EXPECT_EQ(std::get<0>(test), host);
+}
+
+TEST_P(TestHost, JsonDeserialization) {
+	TestHost_testcase_t test = GetParam();
+
+	host host;
+	Json::Value root;
+
+	ASSERT_TRUE(reader.parse(std::get<1>(test), root));
+
+	root >> host;
+
+	EXPECT_EQ(std::get<0>(test), host);
+}
+
+TEST_P(TestHost, JsonSerialization) {
+	TestHost_testcase_t test = GetParam();
+
+	Json::Value root;
+	root << std::get<0>(test);
+
+	EXPECT_EQ(std::get<1>(test), Json::writeString(builder, root));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Addresses, TestHost,
+    ::testing::Values(
+        TestHost_testcase_t{
+            host{.ip = boost::asio::ip::make_address("1.2.3.4")}, R"({"hostnames":[],"ip":"1.2.3.4"})",
+            R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
+        TestHost_testcase_t{
+            host{.ip = boost::asio::ip::make_address("1.2.3.4"), .mac = "01:23:45:67:89:ab"},
+            R"({"hostnames":[],"ip":"1.2.3.4","mac":"01:23:45:67:89:ab"})",
+            R"(<host><status state="up" reason="arp-response" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><address addr="01:23:45:67:89:AB" addrtype="mac"/><hostnames></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"}));
+INSTANTIATE_TEST_SUITE_P(
+    Hostnames, TestHost,
+    ::testing::Values(
+        TestHost_testcase_t{
+            host{.ip = boost::asio::ip::make_address("1.2.3.4"), .hostnames = {"example.com"}},
+            R"({"hostnames":["example.com"],"ip":"1.2.3.4"})",
+            R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames><hostname name="example.com" type="PTR"/></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
+        TestHost_testcase_t{
+            host{.ip = boost::asio::ip::make_address("1.2.3.4"),
+                 .hostnames = {"example.com", "example.local", "aaaaaaaaa"}},
+            R"({"hostnames":["example.com","example.local","aaaaaaaaa"],"ip":"1.2.3.4"})",
+            R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames><hostname name="example.com" type="PTR"/><hostname name="example.local" type="PTR"/><hostname name="aaaaaaaaa" type="PTR"/></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"}));
