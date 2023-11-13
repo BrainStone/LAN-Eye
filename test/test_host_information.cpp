cb57743e46878988c47bf6f6e615033f89611a63
BrainStone <yannick@tedworld.de>
2023-11-13 04:00:11 +0100

    Test XmlDeserialization

--- test/test_host_information.cpp
+++ test/test_host_information.cpp
@@ -1,20 +1,24 @@
 #include <limits>
 #include <string>
-#include <utility>
+#include <tuple>
 
 #include "gtest/gtest.h"
 #include "host_information.hpp"
 #include "json/reader.h"
 #include "json/writer.h"
 
-const std::pair<host, std::string> host_test_cases[]{
-    {host{.ip = boost::asio::ip::make_address("1.2.3.4")}, R"({"hostnames":[],"ip":"1.2.3.4"})"},
+const std::tuple<host, std::string, std::string> host_test_cases[]{
+    {host{.ip = boost::asio::ip::make_address("1.2.3.4")}, R"({"hostnames":[],"ip":"1.2.3.4"})",
+     R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
     {host{.ip = boost::asio::ip::make_address("1.2.3.4"), .mac = "01:23:45:67:89:ab"},
-     R"({"hostnames":[],"ip":"1.2.3.4","mac":"01:23:45:67:89:ab"})"},
+     R"({"hostnames":[],"ip":"1.2.3.4","mac":"01:23:45:67:89:ab"})",
+     R"(<host><status state="up" reason="arp-response" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><address addr="01:23:45:67:89:AB" addrtype="mac"/><hostnames></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
     {host{.ip = boost::asio::ip::make_address("1.2.3.4"), .hostnames = {"example.com"}},
-     R"({"hostnames":["example.com"],"ip":"1.2.3.4"})"},
+     R"({"hostnames":["example.com"],"ip":"1.2.3.4"})",
+     R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames><hostname name="example.com" type="PTR"/></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
     {host{.ip = boost::asio::ip::make_address("1.2.3.4"), .hostnames = {"example.com", "example.local", "aaaaaaaaa"}},
-     R"({"hostnames":["example.com","example.local","aaaaaaaaa"],"ip":"1.2.3.4"})"},
+     R"({"hostnames":["example.com","example.local","aaaaaaaaa"],"ip":"1.2.3.4"})",
+     R"(<host><status state="up" reason="reset" reason_ttl="0"/><address addr="1.2.3.4" addrtype="ipv4"/><hostnames><hostname name="example.com" type="PTR"/><hostname name="example.local" type="PTR"/><hostname name="aaaaaaaaa" type="PTR"/></hostnames><times srtt="123456" rttvar="12345" to="1000000"/></host>)"},
 };
 
 class TestChronoHelpers : public ::testing::TestWithParam<std::intmax_t> {
@@ -59,18 +63,33 @@ TEST(TestChronoHelpers, MakeTimePointNow) {
 	          make_time_point<std::chrono::steady_clock::time_point>(steady_now.time_since_epoch().count()));
 }
 
+TEST(TestHost, XmlDeserialization) {
+	Json::Reader reader;
+
+	for (const std::tuple<host, std::string, std::string>& test : host_test_cases) {
+		host host;
+		pugi::xml_document doc;
+
+		ASSERT_TRUE(doc.load_string(std::get<2>(test).c_str()));
+
+		doc.child("host") >> host;
+
+		EXPECT_EQ(std::get<0>(test), host);
+	}
+}
+
 TEST(TestHost, JsonDeserialization) {
 	Json::Reader reader;
 
-	for (const std::pair<host, std::string>& test : host_test_cases) {
+	for (const std::tuple<host, std::string, std::string>& test : host_test_cases) {
 		host host;
 		Json::Value root;
 
-		ASSERT_TRUE(reader.parse(test.second, root));
+		ASSERT_TRUE(reader.parse(std::get<1>(test), root));
 
 		root >> host;
 
-		EXPECT_EQ(test.first, host);
+		EXPECT_EQ(std::get<0>(test), host);
 	}
 }
 
@@ -78,10 +97,10 @@ TEST(TestHost, JsonSerialization) {
 	Json::StreamWriterBuilder builder{};
 	builder["indentation"] = "";
 
-	for (const std::pair<host, std::string>& test : host_test_cases) {
+	for (const std::tuple<host, std::string, std::string>& test : host_test_cases) {
 		Json::Value root;
-		root << test.first;
+		root << std::get<0>(test);
 
-		EXPECT_EQ(test.second, Json::writeString(builder, root));
+		EXPECT_EQ(std::get<1>(test), Json::writeString(builder, root));
 	}
 }
